b4f2389a12693b39a325a14e1df02dcac80aec39
LayNath242 <nath.selendra@gamail.com>
2025-05-01 07:06:58 +0700

    Consume all available gas when gas limit has been reached

--- primitives/evm/src/lib.rs
+++ primitives/evm/src/lib.rs
@@ -129,11 +129,15 @@ impl WeightInfo {
 		if let (Some(ref_time_usage), Some(ref_time_limit)) =
 			(self.ref_time_usage, self.ref_time_limit)
 		{
-			let ref_time_usage = self.try_consume(cost, ref_time_limit, ref_time_usage)?;
-			if ref_time_usage > ref_time_limit {
-				return Err(ExitError::OutOfGas);
+			match self.try_consume(cost, ref_time_limit, ref_time_usage) {
+				Ok(ref_time_usage) => {
+					self.ref_time_usage = Some(ref_time_usage);
+				}
+				Err(e) => {
+					self.ref_time_usage = Some(ref_time_limit);
+					return Err(e);
+				}
 			}
-			self.ref_time_usage = Some(ref_time_usage);
 		}
 		Ok(())
 	}
@@ -142,12 +146,15 @@ impl WeightInfo {
 		if let (Some(proof_size_usage), Some(proof_size_limit)) =
 			(self.proof_size_usage, self.proof_size_limit)
 		{
-			let proof_size_usage = self.try_consume(cost, proof_size_limit, proof_size_usage)?;
-			if proof_size_usage > proof_size_limit {
-				storage_oog::set_storage_oog();
-				return Err(ExitError::OutOfGas);
+			match self.try_consume(cost, proof_size_limit, proof_size_usage) {
+				Ok(proof_size_usage) => {
+					self.proof_size_usage = Some(proof_size_usage);
+				}
+				Err(e) => {
+					self.proof_size_usage = Some(proof_size_limit);
+					return Err(e);
+				}
 			}
-			self.proof_size_usage = Some(proof_size_usage);
 		}
 		Ok(())
 	}
